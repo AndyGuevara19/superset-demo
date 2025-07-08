074378d0dd70a6dcc31a38e1547cb839497a62b8
Andy Guevara <82056526+AndyGuevara19@users.noreply.github.com>
2025-07-08 11:24:13 -0500

    Add files via upload

--- superset_config.py
+++ superset_config.py
@@ -1,2 +1,8 @@
 import os
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY", "tu_clave_ultra_secreta_123!@#")
+ENABLE_PUBLIC_DASHBOARD = True
+FEATURE_FLAGS = {
+    "EMBEDDED_SUPERSET": True,
+    "ALLOW_JINJA_IN_DASHBOARD_FILTERS": True,
+    "DASHBOARD_RBAC": True,
+}
